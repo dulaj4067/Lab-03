02b14913b2298ed84a50a5b19e80e5351ed661d4
Dulaj Serasinghe <it23599086@my.sliit.lk>
2026-02-23 23:28:16 +0530

    Add async/await feature

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("Hello");
+const myPromise = Promise.resolve('Async success!');
+
+async function myFunction() {
+  try {
+    const result = await myPromise;
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+myFunction();
